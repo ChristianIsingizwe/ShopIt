d2fd44b04540dbae35bfb06076dd47beea417c25
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2025-05-18 01:27:47 +0200

    Ready to go on the cart section

---
+++ src/main/java/com/projects/shopIt/validations/LowercaseValidator.java
@@ -0,0 +1,13 @@
+package com.projects.shopIt.validations;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class LowercaseValidator implements ConstraintValidator<LowerCase, String> {
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
+        if (value == null) return true;
+        return value.equals(value.toLowerCase());
+    }
+}
